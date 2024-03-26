de3ecc9c02e298a934e8caf6fd6f77a03aa9713f
AhmedAshraf14 <a.ashraf.dev@gmail.com>
2024-03-26 16:51:07 +0200

    Initial commit: Set up project structure based on MVVM architecture.

---
+++ banquemisr.challenge05/ViewController/MovieDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailsViewController.swift
+//  banquemisr.challenge05
+//
+//  Created by Ahmed Ashraf on 26/03/2024.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ banquemisr.challenge05/ViewController/MoviesListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesListViewController.swift
+//  banquemisr.challenge05
+//
+//  Created by Ahmed Ashraf on 26/03/2024.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
